7577b1178accbe819a8f2fbbb581f8c06c5ea60c
Naoto Ushizaki <naoto.ushizaki@gmail.com>
2026-03-05 13:53:09 +0000

    ativ3_lab3 add

--- LAB3/pipe_parallel.c
+++ LAB3/pipe_parallel.c
@@ -4,39 +4,86 @@
 #include<sys/wait.h>
 #include<unistd.h>
 
+#define PROCESSES 4
+#define TAM_VETOR 1000
+
+int somador(int *v, int start, int end){
+    int partioned_sum=0;
+    for(int i = start; i < end; i++){
+        partioned_sum += v[i];
+    }
+    return partioned_sum;
+}
+
+void loading_bar(int passo, int total){
+    int width = 20;
+    int preenche = (passo * width) / total;
+    
+    printf("\r[");
+    for(int a = 0; a < preenche; a++) printf("█");
+    for(int b = preenche; b < width; b++) printf(" ");
+    printf("] %d%%", (passo * 100)/total);
+    fflush(stdout);
+}
+
 // Main:
 int main(){
     // Criando as pipes(com os descritores)
-    int pipefd[2];
-    char buffer[100];
+    int pipers[PROCESSES][2];
+    int vector[TAM_VETOR];
+    double soma_total;
+    int vector_piece = TAM_VETOR / PROCESSES;
+    int total = 10;
 
-    if(pipe(pipefd) == -1){
-        perror("Pipe");
-        exit(EXIT_FAILURE);
+    for(int b = 0; b < TAM_VETOR; b++){
+        vector[b] = 1;
     }
 
     // Criando filho:
-    pid_t pid = fork();
-    if(pid < 0){
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    if(pid == 0){
-        close(pipefd[0]); // Não irá ler(read)
+    for(int y = 0; y < PROCESSES; y++){
+        if(pipe(pipers[y]) == -1){
+            perror("Pipe");
+            exit(EXIT_FAILURE);
+        }
+        pid_t pid = fork();
 
-        const char* mensagem = "Message from SON";
-        printf("\nFILHO SENDING MESSAGE...\n");
+        if(pid < 0){
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+        if(pid == 0){
+            close(pipers[y][0]); // Não irá ler(read)
 
-        write(pipefd[1], mensagem, strlen(mensagem + 1));
-        close(pipefd[1]); // Fecha ponta de escrita
-        exit(EXIT_FAILURE);
-    } else {
-        close(pipefd[1]); // Não irá escrever
-        printf("\nPAI: Awaiting message");
-        read(pipefd[0], buffer, sizeof(buffer));
-        printf("\nPAI -> Vox Message received --> %s", buffer);
-        close(pipefd[0]);
-        wait(NULL);
+            int comeco_ind = y * vector_piece;
+            int end_ind = (y == PROCESSES - 1) ? TAM_VETOR : (y + 1) * vector_piece;
+            printf("\nFILHO CALCULALTING SUM...\n");
+            double soma_local = somador(vector, comeco_ind, end_ind); // chama funcao
+            write(pipers[y][1], &soma_local, sizeof(double));
+            close(pipers[y][1]); // Fecha ponta de escrita
+            exit(EXIT_FAILURE);
+        }
+        for(int t = 0; t < PROCESSES; t++){
+            close(pipers[t][1]); // Não irá escrever
+            // Laço pai:
+            double soma_local;
+            printf("\nPAI: Awaiting sum...");
+            /*
+            for(int x = 0; x <= total; x++){
+                loading_bar(x, total);
+                usleep(10000);
+            }
+            */
+            read(pipers[t][0], &soma_local, sizeof(double));
+            soma_total += soma_local;
+            close(pipers[t][0]);
+            wait(NULL);
+        }    
     }
+    for(int x = 0; x <= total; x++){
+        loading_bar(x, total);
+        usleep(300000);
+    }
+    printf("\nPAI -> Vox Sum received --> %.2f", soma_total);
+    
     return 0;
 }
